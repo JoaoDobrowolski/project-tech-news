d2be8be381d94cf594610b88b708c22002cac650
JoaoDobrowolski <joaodobrowolski@outlook.com>
2023-03-14 15:15:03 -0300

    função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    # https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362
+    query = {
+        "title": {
+            "$regex": title,
+            "$options": 'i'}}
+    search = [(index['title'], index['url']) for index in db.news.find(query)]
+    # print(">>>>>>", search)
+    return search
 
 
 # Requisito 8
